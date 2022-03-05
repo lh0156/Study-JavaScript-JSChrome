8de94daa413fa61af21d3279098e7e74ffdc1705
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-03-05 23:38:32 +0900

    add undefine

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const isboolean = true;
 const notIsboolean = false;
 const isnull = null;
+let isUnDefine;
 
 
 console.log(isboolean);
 console.log(notIsboolean);
 console.log(isnull);
+console.log(isUnDefine);
 
