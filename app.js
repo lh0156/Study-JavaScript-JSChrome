193712545a421d07b4fb37556d1ecb7523faa080
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-03-08 23:08:27 +0900

    add prompt

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
-const calculater = {
-  add: function(a, b){
-    return a+b;
-  },
+const age = prompt("How old are you");
 
-  subtract: function(a, b){
-    return a-b;
-  },
-
-  division: function(a, b){
-    return a/b;
-  },
-
-  multiply: function(a, b){
-    return a * b;
-  },
-
-  squared: function(a, b){
-    return a ** b;
-  }
-};
-
-const human = {
-  name: "yunseop",
-  age: "25"
-};
-
-console.log(human.age);
-
-human["age"] = 22;
-
-console.log(human.age);
+console.log(age);
