2c623b67ebda25f78dd8fe43505384fa7c70d266
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-03-09 14:30:47 +0900

    add to pratice

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const age = parseInt(prompt("How old are you"));
 
 if(isNaN(age)){
   console.log("문자입니다.");
-} else {
-  console.log("숫자입니다.");
-}
+} else if (age < 18){
+  console.log("미성년자는 술을 먹을 수 없습니다");
+} else if (age >= 18 && age <= 500{
+  console.log("음주 가능합니다")
+} 
